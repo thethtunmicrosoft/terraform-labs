0e978ae38302b9815ff927f27bba4759104931c2
thethtunaung <thaung@microsoft.com>
2019-10-12 17:28:37 +0630

    ver0.2

--- corenetwork.tf
+++ corenetwork.tf
@@ -11,25 +11,27 @@ resource "azurerm_virtual_network" "core" {
   resource_group_name = "${azurerm_resource_group.coretf.name}"
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["1.1.1.1","1.0.0.1"]
-  
+}
 
-  subnet {
-    name           = "GatewaySubnet"
-    address_prefix = "10.0.0.0/24"
-  }
+resource "azurerm_subnet" "Gatewaysubnet" {
+  name                 = "GatewaySubnet"
+  resource_group_name  = "${azurerm_resource_group.coretf.name}"
+  virtual_network_name = "${azurerm_virtual_network.core.name}"
+  address_prefix       = "10.0.0.0/24"
+}
 
-  subnet {
-    name           = "training"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name           = "dev"
-    address_prefix = "10.0.2.0/24"
-      }
-
-  tags = "${var.tags}"
-  
+resource "azurerm_subnet" "training" {
+  name                 = "training"
+  resource_group_name  = "${azurerm_resource_group.coretf.name}"
+  virtual_network_name = "${azurerm_virtual_network.core.name}"
+  address_prefix       = "10.0.1.0/24"
+}
+ 
+ resource "azurerm_subnet" "dev" {
+  name                 = "dev"
+  resource_group_name  = "${azurerm_resource_group.coretf.name}"
+  virtual_network_name = "${azurerm_virtual_network.core.name}"
+  address_prefix       = "10.0.2.0/24"
 }
 
 resource "azurerm_public_ip" "vpnGatewayPublicIp" {
@@ -45,7 +47,7 @@ resource "azurerm_virtual_network_gateway" "vpnGateway" {
   location = "${var.loc}"
   tags = "${var.tags}"
   resource_group_name = "${azurerm_resource_group.coretf.name}"
-  type = vpn
+  type = "vpn"
   vpn_type = "Routebased"
   enable_bgp = true
   sku = "Basic"
@@ -54,10 +56,7 @@ resource "azurerm_virtual_network_gateway" "vpnGateway" {
       name = "vpnGwConfig1"
       public_ip_address_id = "${azurerm_public_ip.vpnGatewayPublicIp.id}"
       private_ip_address_allocation = "Dynamic"
-      subnet_id = "${azurerm_virtual_network.core.subnet.GatewaySubnet}"
+      subnet_id = "${azurerm_subnet.Gatewaysubnet.id}"
 
-
-  }
 }
-
-
+}
